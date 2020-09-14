2f3d8af816576d0083380d992673b0d15ee37039
joanfonseca <joan.fonseca01@uptc.edu.co>
2020-09-13 19:45:13 -0500

    iniciando proyecto

---
+++ src/main/java/com/asopaps/asopaps/AsopapsApplication.java
@@ -0,0 +1,13 @@
+package com.asopaps.asopaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsopapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsopapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asopaps/asopaps/AsopapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asopaps.asopaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsopapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
