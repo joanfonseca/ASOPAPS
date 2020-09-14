a474334bddd1b50d99dc42260e342ec39dde2369
joanfonseca <joan.fonseca01@uptc.edu.co>
2020-09-13 22:35:21 -0500

    Comentay

---
+++ src/main/java/com/asopaps/asopaps/repository/ComentaryRepository.java
@@ -0,0 +1,9 @@
+package com.duseni.duseni.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.duseni.duseni.entities.Comentary;
+
+public interface ComentaryRepository extends CrudRepository<Comentary, Long>  {
+
+}
