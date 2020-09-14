46b534c61771cf15079d5655bbb598703ea05b48
joanfonseca <joan.fonseca01@uptc.edu.co>
2020-09-13 22:35:05 -0500

    City

---
+++ src/main/java/com/asopaps/asopaps/repository/CityRepository.java
@@ -0,0 +1,11 @@
+package com.duseni.duseni.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.duseni.duseni.entities.City;
+
+public interface CityRepository extends CrudRepository<City, Long>  {
+
+		
+	
+}
